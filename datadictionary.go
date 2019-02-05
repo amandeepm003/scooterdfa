b66b8d5ae03a5b21e0dfa7a32222f8d8158c46c3
Amandeep Midha <amandeep@ernit.com>
2019-02-05 02:18:09 +0100

    Data dictionary first committ

---
+++ datadictionary.go
@@ -0,0 +1,83 @@
+package voidfa
+
+//States of DFA involved
+
+type State int
+
+const (
+	StateReady = iota
+	StateBatteryLow
+	StateBounty
+	StateRiding
+	StateCollected
+	StateDropped
+
+	// Not in service, only admin can claim.
+	StateServiceMode //Workshop
+	StateTerminated  //Dismantled/Destroyed
+	StateUnknown     //After 48 hours of staying ready, could be false status/stolen i.e. tagged Unknown in system
+)
+
+//Roles in scene affecting DFA
+
+type Role int
+const (
+	//External User Roles
+	RoleUser = iota
+	RoleHunter
+	RoleAdmin
+	//Internal Role
+	RoleSysInternal
+)
+
+// Enumerating all events that can happen in DFA, and by whom (and above)
+type Event struct {
+	OutputState State
+	Role  Role
+}
+
+var (
+	EventDeploy = Event {OutputState:StateReady, Role: RoleHunter} //voi is made available in street
+	EventBatteryLowTrigger = Event {OutputState: StateBatteryLow, Role: RoleSysInternal} //not just riding, lying around idle also will decay battery
+	EventHunterAlertedForBounty = Event {OutputState: StateBounty} //zero state transition
+	EventHunterCollected = Event {OutputState:StateCollected, Role:RoleHunter}
+	EventRideStart = Event {OutputState:StateRiding}
+	EventRideComplete = Event {OutputState: StateReady}
+	EventSleep = Event{OutputState: StateBounty, Role: RoleSysInternal}  //At 9.30pm it will go to sleep/bounty mode
+	//NOT IMPLEMENTED -> EventWakeUp = Event {OutputState: StateReady, Role: RoleSysInternal} //There is assumption that morning 7.30 am it will be wake up back to Ready State
+	EventUnknown = Event {OutputState: StateUnknown, Role: RoleSysInternal} //Inactive 48hrs, Tampered, Stolen, Drowned, Anything here
+	EventStandby = Event {OutputState: StateServiceMode, Role: RoleAdmin}
+	EventTerminate = Event {OutputState:StateTerminated, Role:RoleAdmin}
+)
+
+type DFATransition struct {
+	PrevState State
+	NewState State
+	Event Event //Injection place to document/log a transition
+	Roles []Role
+}
+
+var availableTransitions = []DFATransition{
+	//NewState is resultant state, event "could be" most probable cause
+	{PrevState: StateReady, NewState: StateRiding, Event: EventRideStart, Roles: []Role{RoleUser, RoleHunter, RoleAdmin}},
+	{PrevState: StateRiding, NewState: StateReady, Event: EventRideStart, Roles: []Role{RoleUser, RoleHunter, RoleAdmin}},
+	{PrevState: StateRiding, NewState: StateBatteryLow, Event: EventBatteryLowTrigger, Roles: []Role{RoleSysInternal}}, //
+	{PrevState: StateBatteryLow, NewState: StateBounty, Roles: []Role{RoleSysInternal}},
+	{PrevState: StateBounty, NewState: StateCollected, Roles: []Role{RoleHunter}},
+	{PrevState: StateCollected, NewState: StateDropped, Roles: []Role{RoleHunter}},
+
+	{PrevState: StateReady, NewState: StateUnknown, Roles: []Role{RoleSysInternal}},
+	{PrevState: StateDropped, NewState: StateReady, Roles: []Role{RoleHunter}},
+}
+
+
+
+/*
+//Questions/Assumptions
+
+1. There is one impossible state to go from Ready to Unknown state after 48 hours.
+As from Ready, every day at 9.30PM, vehicle goes to Bounty state.
+So there will always be a state change within 1 day and hence the 48 hours will never be achieved ?
+
+//NOT IMPLEMENTED 2. If there is a sleep mode at 9.30pm, there is assumption that morning 7.30 am it will be wake up back to Ready State
+ */
